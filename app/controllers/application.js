c2ee1a2ec05af272f37320d55e89a48f8d873e91
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2015-01-04 15:16:25 -0800

    view hides after a click

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,5 +2,18 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   searchTerm: "",
-  wordList: []
+  showSearchList: false,
+  searchTermObserver: function() {
+    if(this.get("searchTerm") === "") {
+      this.set("showSearchList", false);
+    } else {
+      this.set("showSearchList", true);
+    }
+  }.observes('searchTerm'),
+  actions: {
+    clickWordFromList: function(word) {
+      this.set("showSearchList", false);
+      this.transitionToRoute("word", word);
+    }
+  }
 });
