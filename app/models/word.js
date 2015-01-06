6a2b0be0debd91fda35cfec5ccf445339fba0bb2
Michael JL Catlin <malrase@gmail.com>
2015-01-06 13:04:16 -0800

    Adding some more definitions to make the page longer

--- app/models/word.js
+++ app/models/word.js
@@ -8,9 +8,9 @@ var Word = DS.Model.extend({
 Word.reopenClass({
   FIXTURES: [
     {id: 1, name: "sushi", meanings: [1]},
-    {id: 2, name: "smash", meanings: [2, 3]},
+  {id: 2, name: "smash", meanings: [2, 3, 8]},
     {id: 3, name: "naive", meanings: [4]},
-    {id: 4, name: "run",   meanings: [5, 6]}
+    {id: 4, name: "run",   meanings: [5, 6, 7]}
   ]
 });
 
