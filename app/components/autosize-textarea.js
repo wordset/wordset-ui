6483007f6dc852813af18a21424f559bf4e1ef4b
Michael JL Catlin <malrase@gmail.com>
2015-01-17 11:39:50 -0800

    Adding semicolon

--- app/components/autosize-textarea.js
+++ app/components/autosize-textarea.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.TextArea.extend({
   didInsertElement: function() {
-    var area = Ember.$('#'+this.get('elementId'))
+    var area = Ember.$('#'+this.get('elementId'));
     area.autosize();
   }
 });
