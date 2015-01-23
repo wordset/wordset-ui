1ee46c59b92cabf471700ab5668b3777ded8dec6
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2015-01-23 14:45:43 -0800

    ga tracking

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,5 +4,11 @@ import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 export default Ember.Route.extend(ApplicationRouteMixin).extend({
   model: function() {
     return this.store.find('word_list');
-  }
+  },
+  notifyGoogleAnalytics: function() {
+    return ga('send', 'pageview', {
+      'page': this.get('url'),
+      'title': this.get('url')
+    });
+  }.on('didTransition')
 });
