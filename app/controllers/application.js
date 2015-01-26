bf285ef90ad9d9d0167636273478041103f7f615
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2015-01-26 13:27:30 -0800

    Better search list behaviour

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,14 +3,22 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   searchTerm: "",
   showSearchList: false,
+  searchLoading: false,
   wordList: null,
   searchTermObserver: function() {
     if(this.get("searchTerm") === "") {
       this.set("showSearchList", false);
+      this.set("searchLoading", false);
       this.set("wordList", null);
     } else {
-      this.set("wordList", this.store.find('word_list', this.get('searchTerm')));
-      this.set("showSearchList", true);
+      this.set("searchLoading", true);
+      var _this = this;
+      this.store.find('word_list', this.get('searchTerm'))
+        .then(function(wordList) {
+          _this.set("showSearchList", true);
+          _this.set("searchLoading", false);
+          _this.set("wordList", wordList);
+        });
     }
   }.observes('searchTerm'),
   actions: {
