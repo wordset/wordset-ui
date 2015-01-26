64e3bd915b67a08a4ea0578fc58f081cc5e6de55
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2015-01-26 13:51:07 -0800

    Lazy load word_list

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin).extend({
-  model: function() {
+  activate: function() {
     return this.store.find('word_list');
   },
   notifyGoogleAnalytics: function() {
