7586c8423ddb87ea4ed9e568af1ca486ea612c60
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2015-02-03 10:33:46 -0800

    MOAR PROPOSALLZL

--- app/routes/proposals.js
+++ app/routes/proposals.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    return this.store.find('proposal');
+  setupController: function(controller) {
+    controller.set('model', this.store.find('proposal'));
   }
 });
