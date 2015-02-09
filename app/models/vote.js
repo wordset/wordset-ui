c2e404b75ba8c01c54cd8fc375be2d7115be26f9
Michael JL Catlin <malrase@gmail.com>
2015-02-09 09:45:40 -0800

    Vote model

---
+++ app/models/vote.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  proposal: DS.belongsTo("proposal"),
+  user: DS.belongsTo("user"),
+  yae: DS.attr("boolean"),
+  value: DS.attr("number"),
+  flagged: DS.attr("boolean")
+});
