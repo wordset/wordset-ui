f0143cfa9534be7c075201d407f9c696114ba175
Michael JL Catlin <malrase@gmail.com>
2015-02-09 17:29:11 -0800

    moving the user index into its own file

--- app/routes/user/proposals.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model: function() {
-    var user = this.modelFor('user');
-    return this.store.find('proposal', {user_id: user.get("id")});
-  },
-});
