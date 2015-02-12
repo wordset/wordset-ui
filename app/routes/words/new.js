655dffbfae713b1addf9fec1071e213bcb91a2d3
Michael JL Catlin <malrase@gmail.com>
2015-02-12 13:36:34 -0800

    Styles when multiple meanings

--- app/routes/words/new.js
+++ app/routes/words/new.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
+  model: function(params) {
     return this.store.createRecord('proposal', {
       type: 'NewWord',
+      wordName: params.wordName,
       meanings: [{}]
     });
   }
