69d2cd5e349dc54c264ceb019c514d72ea9283ef
Michael JL Catlin <malrase@gmail.com>
2015-02-12 09:28:52 -0800

    starting the process for new words

--- app/controllers/words/new.js
+++ app/controllers/words/new.js
@@ -3,4 +3,12 @@ import Ember from "ember";
 
 export default Ember.ObjectController.extend({
   posList: ["adv", "adj", "verb", "noun"],
+
+  actions: {
+    submitProposal: function() {
+      console.log(this.get("proposalTarget"));
+    }
+  }
+
+
 });
