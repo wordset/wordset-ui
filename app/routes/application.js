451a9e25f14315bf10d32d72f8cc63d3cd52ed57
Michael JL Catlin <malrase@gmail.com>
2015-02-16 13:54:54 -0800

    Only talk to GA and mixpanel when in production

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
+import ENV from '../config/environment';
 /* global ga */
 /* global mixpanel */
 
@@ -7,22 +8,26 @@ export default Ember.Route.extend(ApplicationRouteMixin).extend({
   activate: function() {
     return this.store.find('word_list');
   },
+  notifyGoogleAnalytics: function() {
+
+  }.on('didTransition'),
+  notifyMixpanel: function(){
+
+    console.log(this.get("url"))
+  }.on('didTransition'),
   actions: {
     willTransition: function() {
       this.controller.set("showMenu", false);
       Ember.$(document).attr('title', 'Wordset – the Collaborative Dictionary');
     },
     didTransition: function(paths) {
-      mixpanel.track("pageview", {"url": window.location.pathname });
-      var user = this.get("session").get("account_id");
-      mixpanel.identify(user);
-      mixpanel.people.set({
-        '$name': user,
-      });
-      ga('send', 'pageview', {
-        'page': window.location.pathname,
-        'title': document.title,
-      });
+      if (ENV.environment === 'production') {
+        mixpanel.track("pageview", {"url": window.location.pathname });
+        ga('send', 'pageview', {
+          'page': window.location.href,
+          'title': document.title,
+        });
+      }
     }
   }
 });
