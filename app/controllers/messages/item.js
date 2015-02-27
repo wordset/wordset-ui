d7f1f7faf377b72eb78b9037a35f6f93e38c40b7
Michael JL Catlin <malrase@gmail.com>
2015-02-26 16:53:26 -0800

    Messages are styled and typed

--- app/controllers/messages/item.js
+++ app/controllers/messages/item.js
@@ -4,4 +4,17 @@ export default Ember.ObjectController.extend({
   isSelf: function() {
     return ("self" === this.get("type"));
   }.property("type"),
+  isLink: function() {
+    return ("link" === this.get("type"));
+  }.property("type"),
+  isSay: function() {
+    return ("say" === this.get("type"));
+  }.property("type"),
+  actions: {
+    goToLink: function() {
+      if(this.get("url")) {
+        this.transitionToRoute(this.get("url"));
+      }
+    },
+  },
 });
