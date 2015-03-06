10b079eea4d0320a5daa3d590a9b22525b0d215a
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2015-03-06 08:35:30 -0800

    whoops

--- app/controllers/users/new.js
+++ app/controllers/users/new.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 import EmberValidations from 'ember-validations';
-import ENV from '../config/environment';
 
 export default Ember.ObjectController.extend( EmberValidations.Mixin, {
   validations: {
