5047f4f92829d2f7b952567edd17046c2fc48b9e
Michael JL Catlin <malrase@gmail.com>
2015-03-11 12:28:39 -0700

    Activities limited to 25 on homepage

--- app/controllers/dashboard/activities.js
+++ app/controllers/dashboard/activities.js
@@ -3,4 +3,7 @@ import Ember from "ember";
 export default Ember.ArrayController.extend({
   sortProperties: ['createdAt'],
   sortAscending: false,
+  limitedActivities: function() {
+    return this.get("arrangedContent").slice(0,24);
+  }.property("arrangedContent.@each"),
 });
