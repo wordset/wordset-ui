6581f51c424951daa6a04af34e57f66e76e624df
Michael JL Catlin <malrase@gmail.com>
2015-03-12 09:25:40 -0700

    VArious places we needed to use 'model'

--- app/controllers/project/index.js
+++ app/controllers/project/index.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
   needs: ['application'],
   currentUser: Ember.computed.alias('controllers.application.currentUser'),
 
--- app/controllers/word.js
+++ app/controllers/word.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
   showBanner: function() {
     return !this.get("session").get("isAuthenticated");
   }.property("session.isAuthenticated"),
