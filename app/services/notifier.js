3db1fb34e63013dd0042ca5da268ae9e79a31f86
Michael JL Catlin <malrase@gmail.com>
2015-03-16 13:51:16 -0700

    Creating the notification service and component
    Making a component for notifications.

---
+++ app/services/notifier.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+
+  notifications: [],
+
+  show: function(text, options) {
+    if(typeof options !== "object") {
+      options = {};
+    }
+    options.text = text;
+
+    this.get("notifications").addObject(options);
+
+    Ember.run.later(this, function() {
+      this.get("notifications").removeObject(options);
+    }, 2500);
+
+  }
+});
