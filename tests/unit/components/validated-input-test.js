eec8f7412ec4a5b250896c2b16de6caf6ee537bc
Drew Chandler <andrewschandler@gmail.com>
2015-03-18 12:02:34 -0700

    s/validate/validated/

--- tests/unit/components/validated-input-test.js
+++ tests/unit/components/validated-input-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleForComponent('validate-input', {
+moduleForComponent('validated-input', {
   // specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
 });
