17a2d706982a4b2b2adaeed23e4f7898b5ce4e42
Michael JL Catlin <malrase@gmail.com>
2015-03-27 11:58:14 -0400

    Messing around with confirm buttons

--- app/components/confirm-button.js
+++ app/components/confirm-button.js
@@ -4,7 +4,6 @@ export default Ember.Component.extend({
   isConfirm: false,
   actions: {
     showConfirm: function() {
-      console.log("Show confirm message");
       this.set("isConfirm", true);
     },
   }
