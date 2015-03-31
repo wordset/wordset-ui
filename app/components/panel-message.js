c6c847cc481352ddbe83942b215c5287f7246973
Hampton Catlin <hcatlin@gmail.com>
2015-03-31 16:37:11 -0400

    fix links in messages

--- app/components/panel-message.js
+++ app/components/panel-message.js
@@ -6,4 +6,11 @@ export default Ember.Component.extend({
   messagedAt: function() {
     return moment(this.get("message.createdAt")).fromNow();
   }.property("message.createdAt", "hup.at"),
+  actions: {
+    goToLink: function() {
+      console.log("linked");
+      this.get("targetObject").send("log", "messages", "clickedlink");
+      this.get("targetObject").transitionToRoute(this.get("message.url"));
+    },
+  },
 });
