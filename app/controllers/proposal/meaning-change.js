a405e6ea82079a8d0dcf97ddabeffb6144c43a21
Michael JL Catlin <malrase@gmail.com>
2015-03-31 16:13:27 -0400

    Meaning change controller doesn't need validations any more
    (The validations are now in the component)

--- app/controllers/proposal/meaning-change.js
+++ app/controllers/proposal/meaning-change.js
@@ -1,16 +1,5 @@
 import Ember from 'ember';
-import EmberValidations from 'ember-validations';
 
-export default Ember.Controller.extend( EmberValidations.Mixin, {
+export default Ember.Controller.extend({
 
-  validations: {
-    def: {
-      presence: true,
-      length: { minimum: 10 }
-    },
-    example: {
-      presence: true,
-      length: { minimum: 10 }
-    },
-  },
 });
