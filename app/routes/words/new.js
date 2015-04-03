3619b3f2e06e4136635239bf25ebaf2a3f3804e4
Michael JL Catlin <malrase@gmail.com>
2015-04-03 09:56:05 -0400

    Deleting unnecessary query params stuff

--- app/routes/words/new.js
+++ app/routes/words/new.js
@@ -1,15 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  queryParams: {
-    wordName: {
-      refreshModel: true
-    }
-  },
   model: function(params) {
     return this.store.createRecord('proposal', {
       type: 'NewWord',
-      wordName: params.wordName,
       meanings: [{}]
     });
   }
