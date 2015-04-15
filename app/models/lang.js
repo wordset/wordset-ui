0fe17bc859fe04a29430d270a0be1402d3ff7933
Michael JL Catlin <malrase@gmail.com>
2015-04-15 10:29:30 -0700

    New relationships between models for meaning, lang and wordset

--- app/models/lang.js
+++ app/models/lang.js
@@ -3,4 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   seqs: DS.hasMany("seqs"),
   proposals: DS.hasMany("proposals"),
+  name: DS.attr("string"),
+  parts: DS.attr(),
 });
