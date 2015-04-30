b4d982dce65d2ee73324d8f4fadc308625802f92
Michael JL Catlin <malrase@gmail.com>
2015-04-30 12:15:33 -0700

    Remove the unused wordset controller

--- app/controllers/wordset.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  showBanner: function() {
-    return !this.get("session").get("isAuthenticated");
-  }.property("session.isAuthenticated"),
-});
