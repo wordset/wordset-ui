c276785d4464bf61cc2afddfe3ddfbc5e598a62d
Hampton Catlin <hcatlin@gmail.com>
2015-04-30 16:37:01 -0700

    Allow customized id reading by label-selectors

--- app/components/label-box.js
+++ app/components/label-box.js
@@ -2,13 +2,20 @@ import Ember from "ember";
 
 export default Ember.Component.extend({
   setup: function() {
-    this.set("isChecked", this.get("selectedLabels").contains(this.get("label")));
+    this.set("isChecked", this.get("selectedLabels").contains(this.get("labelKey")));
   }.on("willInsertElement"),
+  labelKey: function() {
+    if(this.get("useId")) {
+      return this.get("label.id");
+    } else {
+      return this.get("label");
+    }
+  }.property("label"),
   updateSelection: function() {
     if(this.get("isChecked") == true) {
-      this.get("selectedLabels").addObject(this.get("label"));
+      this.get("selectedLabels").addObject(this.get("labelKey"));
     } else if(this.get("isChecked") == false ) {
-      this.get("selectedLabels").removeObject(this.get("label"));
+      this.get("selectedLabels").removeObject(this.get("labelKey"));
     };
   }.observes("isChecked")
 });
