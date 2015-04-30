26262f5a552f3434fc236cbf57563e411a7c20bb
Michael JL Catlin <malrase@gmail.com>
2015-04-30 12:15:50 -0700

    Seq controlled needs the showBanner function
    The banner should show up if you're not signed in

--- app/controllers/seq.js
+++ app/controllers/seq.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-
+  showBanner: function() {
+    return !this.get("session").get("isAuthenticated");
+  }.property("session.isAuthenticated"),
 });
