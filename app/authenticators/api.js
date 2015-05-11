b1edd2fa896508b15e1ec44f4271ab0e59e4046c
Hampton Catlin <hcatlin@gmail.com>
2015-05-11 15:01:36 -0700

    Check Session Validity During Restore
    This should fix #36

--- app/authenticators/api.js
+++ app/authenticators/api.js
@@ -5,10 +5,18 @@ import ENV from 'wordset/config/environment';
 export default Base.extend({
   restore: function(properties) {
     var propertiesObject = Ember.Object.create(properties);
-
     return new Ember.RSVP.Promise(function(resolve, reject) {
       if (!Ember.isEmpty(propertiesObject.get("username")) && !Ember.isEmpty(propertiesObject.get("auth_key"))) {
-        resolve(properties);
+        var _this = this;
+        Ember.$.post(`${ENV.api}/auth/authorized`, {username: propertiesObject.get("username"),
+                                                   auth_key: propertiesObject.get("auth_key") }
+                    ).then(function(response) {
+          if(response === true) {
+            resolve(properties);
+          } else {
+            reject();
+          }
+        });
       } else {
         reject();
       }
