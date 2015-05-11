8edc227ce4ccf0f7aa8145158a911023aba6c36a
Hampton Catlin <hcatlin@gmail.com>
2015-05-11 15:02:27 -0700

    Fix a bug with FB redirects
    Was breaking FB login.

--- app/routes/user.js
+++ app/routes/user.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   setupController: function(controller, user) {
     this._super(controller, user);
-    this.store.find('proposal', {user_id: user.get("id")});
+    this.store.find('proposal', {user_id: user.id});
   },
-  
+
 });
