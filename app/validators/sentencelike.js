9613da9e4f7fdcf0d4d042ec33e7edcae3f00e7b
Michael JL Catlin <malrase@gmail.com>
2015-05-14 11:37:12 -0700

    Fix the sentencelike code so it works
    (1) Move the vars into the else statement so they don't get caught on the isBlank thing
    (2) Try to catch extra space before no puntuation

--- app/validators/sentencelike.js
+++ app/validators/sentencelike.js
@@ -5,8 +5,6 @@ export default Base.extend({
     var prop = this.model.get(this.property);
 
     var endingPunctuation = [".", "?", "!"]
-    var hasPunctuation = endingPunctuation.contains(prop[prop.length - 1]);
-    var hasPunctuationAndExtraSpace = endingPunctuation.contains(prop[prop.length - 2]) && prop[prop.length -1] === " ";
 
     if (Ember.isBlank(prop)) {
       this.errors.pushObject("We need something here!");
@@ -14,10 +12,14 @@ export default Base.extend({
       this.errors.pushObject("Start your example sentence with a capital letter.");
     } else if ( (prop[0] || prop[prop.length - 1]) == "\"") {
       this.errors.pushObject("There's no need to use quotation marks.");
-    } else if (!hasPunctuation) {
-      this.errors.pushObject("Finish your example sentence with a full stop, question mark, or exclamation point.");
-    } else if (hasPunctuationAndExtraSpace) {
-      this.errors.pushObject("You have an extra space or you are trying to write two sentences.")
+    } else {
+      var hasPunctuation = endingPunctuation.contains(prop[prop.length - 1]);
+      var hasPunctuationAndExtraSpace = endingPunctuation.contains(prop[prop.length - 2]) && prop[prop.length -1] === " ";
+      if (hasPunctuationAndExtraSpace) {
+        this.errors.pushObject("You have an extra space or you are trying to write two sentences.")
+      } else if (!hasPunctuation) {
+        this.errors.pushObject("Finish your example sentence with a full stop, question mark, or exclamation point.");
+      }
     }
   }
 });
