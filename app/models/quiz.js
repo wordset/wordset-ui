06c7af8c78d6e2f5bb408a1cdf84bfd3e1e4b90a
Michael JL Catlin <malrase@gmail.com>
2015-05-20 09:57:52 -0700

    Add image fields to the quiz model

--- app/models/quiz.js
+++ app/models/quiz.js
@@ -4,6 +4,8 @@ export default DS.Model.extend({
   lang: DS.belongsTo("lang"),
   title: DS.attr("string"),
   image_url: DS.attr("string"),
+  image_citation: DS.attr("string"),
+  image_link: DS.attr("string"),
   instructions: DS.attr("string"),
   questions: DS.attr(),
   results: DS.attr(),
