824cfc6fe56afad9f4fe06df61a682282a5ac54d
Michael JL Catlin <malrase@gmail.com>
2015-05-27 14:46:48 -0700

    Trying to get posts to show up in component

--- app/components/latest-blog-posts.js
+++ app/components/latest-blog-posts.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+  willInsertElement: function() {
+    var _this = this;
+    this.get("targetObject.store").find("post").then( function(post) {
+      controller.set("model", post);
+    }, function(){} );
+  },
 });
