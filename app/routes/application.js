1f80185f88824e2977eefc62d2ae5d9b2b446b35
Michael JL Catlin <malrase@gmail.com>
2015-05-27 09:43:09 -0700

    Fix misspelled "global" and remove unused variable

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,7 +3,7 @@ import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 import ENV from '../config/environment';
 import Proposal from '../models/proposal';
 /* global ga */
-/* gloabl NREUM */
+/* global NREUM */
 
 export default Ember.Route.extend(ApplicationRouteMixin).extend({
   notifier: Ember.inject.service(),
@@ -69,7 +69,6 @@ export default Ember.Route.extend(ApplicationRouteMixin).extend({
       });
     },
     log: function(category, name) {
-      var metaData = {"url": window.location.pathname, "user": this.get("session").get("username")};
       if(ENV.environment === "production") {
         Ember.run(function() {
           ga('send', 'event', category, name);
