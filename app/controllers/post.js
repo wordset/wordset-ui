7b808a900d7d8ea4fddcf4460760c12effd2cb43
Hampton Catlin <hcatlin@gmail.com>
2015-06-01 12:21:28 -0700

    get latest posts

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -2,10 +2,14 @@ import Ember from 'ember';
 /* global moment */
 
 export default Ember.Controller.extend({
+  needs: ['application'],
   html: function() {
     return Ember.String.htmlSafe(this.get("model.text"));
   }.property("model.text"),
   formattedDate: function() {
     return moment(this.get('model.publishedAt')).format("LL");
   }.property("model.publishedAt"),
+  //activeProject: Ember.computed.alias("currentLang.project"),
+  currentLang: Ember.computed.alias("controllers.application.currentLang"),
+  postsSimple: Ember.computed.alias("currentLang.postsSimple"),
 });
