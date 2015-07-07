c7bca6e0c55aaec465dbd92033e4f743cc6f2237
Hampton Catlin <hcatlin@gmail.com>
2015-07-07 14:52:54 -0700

    you can now click on chat links and open a new tab easily

--- app/components/panel-message.js
+++ app/components/panel-message.js
@@ -6,11 +6,4 @@ export default Ember.Component.extend({
   messagedAt: function() {
     return moment(this.get("message.createdAt")).fromNow();
   }.property("message.createdAt", "hup.at"),
-  actions: {
-    goToLink: function() {
-      console.log("linked");
-      this.get("targetObject").send("log", "messages", "clickedlink");
-      this.get("targetObject").transitionToRoute(this.get("message.url"));
-    },
-  },
 });
--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 export default Router.map(function() {
   this.resource("index", {path: "/"});
+  this.route("redirector");
 
   this.route("login");
   this.route("user", {path: "/user/:user_id"}, function() {
