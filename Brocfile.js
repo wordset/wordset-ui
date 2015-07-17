5782f5c3a26ee1e8b32ec74ee91fcb36cc3b29f5
Kaelig <kaelig@deloumeau.fr>
2015-07-17 14:12:22 -0700

    Improve file compression and automate vendor prefixing

--- Brocfile.js
+++ Brocfile.js
@@ -5,6 +5,16 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp({
   fingerprint: {
     exclude: ['images/badges']
+  },
+  autoprefixer: {
+    cascade: false
+  },
+  minifyCSS: {
+    enabled: true,
+    options: {
+      aggressiveMerging: false,
+      advanced: false
+    }
   }
 });
 
