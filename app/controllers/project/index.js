6c12bd0b2f21a405be47ba13999bc27c085494f2
Michael JL Catlin <malrase@gmail.com>
2015-07-22 11:03:36 -0700

    Remove unnecessary project descriptions

--- app/controllers/project/index.js
+++ app/controllers/project/index.js
@@ -8,14 +8,6 @@ export default Ember.Controller.extend({
     return Ember.String.htmlSafe(this.get("model.longDescription"));
   }.property("model.longDescription"),
 
-  description: function() {
-    if(this.get("model.name") === "Proper Noun Purge") {
-      return "<p>We're starting to implement projects on our site. These are self-contained groups of improvements we want to make to Wordset.</p><p>Our first major project is to remove proper nouns from Wordset. We believe that our dictionary should be about definitions, not reference. A dictionary is not the place to gather in-depth information about a person or place. That's what an encyclopedia is for.</p>".htmlSafe();
-    } else if(this.get("model.name") === "Parentheses Roundup") {
-      return "<p>Our next major project is to remove parentheses from definitions. You'll have seen them everywhere – '(in law)', '(slang)', and so on.</p><p>The information contained within the parentheses is useful, but the parentheses themselves break up the definition too much. We want to put the parenthetical back into the definition so it reads more smoothly.</p><p>To help out, you'll be directed to a definition with some parentheses. Just edit the definition to remove them and submit it!</p>".htmlSafe();
-    }
-  }.property("model.name"),
-
   isComplete: function() {
     return (this.get("model.state") === "completed");
   }.property("model.state"),
@@ -25,7 +17,7 @@ export default Ember.Controller.extend({
       return "randomProposal";
     }
     return "randomTarget";
-  }.property("model.name"),
+  }.property("model"),
 
   actions: {
     signUp: function() {
