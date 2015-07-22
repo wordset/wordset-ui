3eace736a0c8ee1428cf07a1ae0ee386c254b44d
Hampton Catlin <hcatlin@gmail.com>
2015-07-22 12:45:34 -0700

    Check for "to " with verbs

--- app/validators/definitionlike.js
+++ app/validators/definitionlike.js
@@ -3,12 +3,15 @@ import Base from 'ember-validations/validators/base';
 
 export default Base.extend({
   call: function() {
+    var pos = (this.model.get("model.pos.pos") || this.model.get("model.meaning.pos"));
     var prop = this.model.get(this.property);
     var backtick = "`";
     var endingPunctuation = [".", "?", "!"];
 
     if (Ember.isBlank(prop)) {
       this.errors.pushObject("We need something here!");
+    } else if ((pos === "verb") && (prop.substr(0,3) !== "to ")) {
+      this.errors.pushObject("Verb definitions should start with 'to '");
     } else if (prop[0] === prop[0].toUpperCase()) {
       this.errors.pushObject("Start your definition with a lowercase letter.");
     } else if (endingPunctuation.contains(prop[prop.length - 1])) {
