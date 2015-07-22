bb4b6ac2b17c1db0068cd426e0a4b46651b43aa8
Hampton Catlin <hcatlin@gmail.com>
2015-07-22 11:25:44 -0700

    add in project countdown ugliness

--- app/controllers/projects.js
+++ app/controllers/projects.js
@@ -3,15 +3,15 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   activeProjects: function() {
-    return this.get("model").filterBy("state", "active")
+    return this.get("model").filterBy("state", "active");
   }.property("model.@each"),
 
   completedProjects: function() {
-    return this.get("model").filterBy("state", "completed")
+    return this.get("model").filterBy("state", "completed");
   }.property("model.@each"),
 
   upcomingProjects: function() {
-    return this.get("model").filterBy("state", "upcoming")
+    return this.get("model").filterBy("state", "upcoming");
   }.property("model.@each"),
 
 
