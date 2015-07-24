bfe8fd7b32ff927a08c32da89d63c2d8f55f5f6a
Michael JL Catlin <malrase@gmail.com>
2015-07-24 16:41:55 -0700

    Panel should switch between two subpanels

--- app/controllers/panel.js
+++ app/controllers/panel.js
@@ -4,4 +4,12 @@ export default Ember.Controller.extend({
   needs: ["application"],
   currentUser: Ember.computed.alias('controllers.application.currentUser'),
   showPanel: Ember.computed.alias('controllers.application.showPanel'),
+
+  activeSubpanel: "chat",
+
+  actions: {
+    showSubpanel: function(panelName) {
+      this.set("activeSubpanel", panelName);
+    },
+  }
 });
