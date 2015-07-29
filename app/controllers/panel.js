fd4d00b2342bbdadb216353a9c9cc8b07bb449e6
Michael JL Catlin <malrase@gmail.com>
2015-07-29 12:26:32 -0700

    Need Ember alias on the panel controller

--- app/controllers/panel.js
+++ app/controllers/panel.js
@@ -4,7 +4,7 @@ export default Ember.Controller.extend({
   needs: ["application"],
   currentUser: Ember.computed.alias('controllers.application.currentUser'),
   showPanel: Ember.computed.alias('controllers.application.showPanel'),
-
+  loggedIn: Ember.computed.alias('controllers.application.loggedIn'),
   activeSubpanel: "chat",
 
   actions: {
