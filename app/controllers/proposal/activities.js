d135b87a9d5c5e2d06b99a860269ddcd9299e8c8
Hampton Catlin <hcatlin@gmail.com>
2015-08-14 14:42:02 -0700

    Proposal activities are simpler
    • Removed subcontrollers, now a component
    • Uses the activitiesSimple set that comes with a proposal

--- app/controllers/proposal/activities.js
+++
@@ -1,7 +0,0 @@
-import Ember from "ember";
-
-export default Ember.Controller.extend({
-  activities: Ember.computed('model.[]', function() {
-    return this.get('model').sortBy('createdAt').reverse();
-  }),
-});
