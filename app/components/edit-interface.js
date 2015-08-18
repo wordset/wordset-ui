592856b8471d8d13ae568e112ec456ad55feadf5
Hampton Catlin <hcatlin@gmail.com>
2015-08-18 15:05:52 -0700

    Add new meaning and add new seq buttons

--- app/components/edit-interface.js
+++ app/components/edit-interface.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-
+export default Ember.Component.extend(EmberValidations, {
+  actions: {
+    addNewMeaning() {
+      this.get("changes.meanings").pushObject(new Object({action: "add", def: "", example: "", labels: []}));
+    },
+    addNewSeq() {
+      this.get("changes.seqs").pushObject(new Object({action: "add", text: "", labels: []}));
+    },
+  }
 });
