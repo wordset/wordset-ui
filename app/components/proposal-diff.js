b3ebe71be324015cc398da6ea56f4493fdfefae8
Michael JL Catlin <malrase@gmail.com>
2015-08-21 14:18:58 -0700

    Update proposal page to also group by pos

--- app/components/proposal-diff.js
+++ app/components/proposal-diff.js
@@ -4,4 +4,19 @@ export default Ember.Component.extend({
   seqs: Ember.computed.alias("proposal.changes.seqs"),
   meanings: Ember.computed.alias("proposal.changes.meanings"),
 
+  posListAndMeanings: Ember.computed("proposal.changes.meanings.[]", function() {
+    var results = {};
+
+    this.get("proposal.changes.meanings.[]").forEach( function(meaning) {
+      var pos = meaning.pos;
+      results[pos] = results[pos] || [];
+      return results[pos].push(meaning);
+    });
+    var posList = [];
+    Object.keys(results).forEach(function(pos) {
+      posList.pushObject([pos, results[pos]]);
+    })
+    return posList;
+  }),
+
 });
