2b705641dca6457754742f0dc3d619df4c7badc3
Michael JL Catlin <malrase@gmail.com>
2015-08-21 13:44:23 -0700

    Sorted meanings

--- app/controllers/seq/wordset/index.js
+++ app/controllers/seq/wordset/index.js
@@ -3,4 +3,14 @@ import Ember from "ember";
 export default Ember.Controller.extend({
   showMeaningProposal: false,
   newMeaningProposal: false,
+
+  sortedMeanings: Ember.computed("model.meanings.[]", function() {
+    var results = {};
+    this.get("model.meanings.[]").forEach( function(meaning) {
+      var pos = meaning.get("pos");
+      results[pos] = results[pos] || [];
+      return results[pos].push(meaning);
+    });
+    return results;
+  }),
 });
