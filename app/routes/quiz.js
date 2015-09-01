b0ad1cd170f5a5d6b70b3ee5b93f24ce516cd1d9
Michael JL Catlin <malrase@gmail.com>
2015-09-01 12:30:54 -0700

    Need to inject the meta service for it to work

--- app/routes/quiz.js
+++ app/routes/quiz.js
@@ -1,14 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  meta: Ember.inject.service(),
   model(params) {
     return this.store.find('quiz', params.id);
   },
   setupController(controller, model) {
     this._super(controller, model);
     controller.set("selections", {});
-
-    this.set("meta.title", 'Quiz: ' + model.get("title"));
     if(!Ember.isBlank(model.get('instructions'))) {
       this.set("meta.description", model.get("instructions"));
     }
