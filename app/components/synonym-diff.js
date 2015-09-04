626a9dc0b55d5b88663ffeac060c7380eece4dab
Michael JL Catlin <malrase@gmail.com>
2015-09-03 19:28:50 -0700

    blame @hcatlin if this breaks it

--- app/components/synonym-diff.js
+++ app/components/synonym-diff.js
@@ -3,23 +3,26 @@ import Ember from "ember";
 export default Ember.Component.extend({
   store: Ember.inject.service("store"),
   addedSynonyms: Ember.computed("newSynonyms.[]", "originalSynonyms.[]", function() {
-    var _this = this;
-    return this.get("newSynonyms").filter(function(item) {
-      return !_this.get("originalSynonyms").contains(item);
+    return this.get("newSynonyms").filter((item) => {
+      return !this.get("originalSynonyms").contains(item);
     });
+  }),
+  newSynonymIds: Ember.computed("newSynonyms.[]", function() {
+    return this.get("newSynonyms").mapBy("meaning_id");
+  }),
+  originalSynonymIds: Ember.computed("originalSynonyms.[]", function() {
+    return this.get("originalSynonym").mapBy("meaning_id");
   }),
 
   removedSynonyms: Ember.computed("newSynonyms.[]", "originalSynonyms.[]", function() {
-    var _this = this;
-    return this.get("originalSynonyms").filter(function(item) {
-      return !_this.get("newSynonyms").contains(item);
+    return this.get("originalSynonyms").filter((item) => {
+      return !this.get("newSynonymIds").contains(item.meaning_id);
     });
   }),
 
   sameSynonyms: Ember.computed("newSynonyms.[]", "originalSynonyms.[]", function() {
-    var _this = this;
-    return this.get("newSynonyms").filter(function(item) {
-      return _this.get("originalSynonyms").contains(item);
+    return this.get("newSynonyms").filter((item) => {
+      return this.get("originalSynonyms").contains(item.meaning_id);
     });
   })
 });
