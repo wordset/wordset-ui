3293a63fcde626ca60a24535563814d29c197433
Michael JL Catlin <malrase@gmail.com>
2015-09-08 12:00:00 -0700

    Fix up synonym diff

--- app/components/synonym-diff.js
+++ app/components/synonym-diff.js
@@ -2,27 +2,27 @@ import Ember from "ember";
 
 export default Ember.Component.extend({
   store: Ember.inject.service("store"),
-  addedSynonyms: Ember.computed("newSynonyms.[]", "originalSynonyms.[]", function() {
-    return this.get("newSynonyms").filter((item) => {
-      return !this.get("originalSynonyms").contains(item);
+  addedSynonyms: Ember.computed("meaning.synonyms.[]", "originalSynonyms.[]", function() {
+    return this.get("meaning.synonyms").filter((item) => {
+      return !this.get("originalSynonymIds").contains(item.meaning_id);
     });
   }),
-  newSynonymIds: Ember.computed("newSynonyms.[]", function() {
-    return this.get("newSynonyms").mapBy("meaning_id");
+  newSynonymIds: Ember.computed("meaning.synonyms.[]", function() {
+    return this.get("meaning.synonyms").mapBy("meaning_id");
   }),
   originalSynonymIds: Ember.computed("originalSynonyms.[]", function() {
-    return this.get("originalSynonym").mapBy("meaning_id");
+    return this.get("originalSynonyms").mapBy("meaning_id");
   }),
 
-  removedSynonyms: Ember.computed("newSynonyms.[]", "originalSynonyms.[]", function() {
+  removedSynonyms: Ember.computed("meaning.synonyms.[]", "originalSynonyms.[]", function() {
     return this.get("originalSynonyms").filter((item) => {
       return !this.get("newSynonymIds").contains(item.meaning_id);
     });
   }),
 
-  sameSynonyms: Ember.computed("newSynonyms.[]", "originalSynonyms.[]", function() {
-    return this.get("newSynonyms").filter((item) => {
-      return this.get("originalSynonyms").contains(item.meaning_id);
+  sameSynonyms: Ember.computed("meaning.synonyms.[]", "originalSynonyms.[]", function() {
+    return this.get("meaning.synonyms").filter((item) => {
+      return this.get("originalSynonymIds").contains(item.meaning_id);
     });
   })
 });
