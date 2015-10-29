5bbd3cacb6dc1bac8b35287c5648d461211d9164
Hampton Catlin <hcatlin@gmail.com>
2015-10-29 08:10:30 -0700

    fixed bug where users page wasnt loading fresh data

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('user', params.user_id, { reload: true });
+  },
   setupController(controller, user) {
     this._super(controller, user);
     this.store.find('proposal', {user_id: user.id});
   },
-
 });
